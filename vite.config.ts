2c144b7015ab6055eb8e37b62447356135da2538
Henri Valette <henri.valette.fr@gmail.com>
2022-03-16 22:10:18 +0100

    fix: page routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,45 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import Pages from 'vite-plugin-pages';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    Pages(),
-    VitePWA({
-      base: '/',
-      includeAssets: [
-        'favicon.svg',
-        'favicon.ico',
-        'robots.txt',
-        'apple-touch-icon.png',
-      ],
-      manifest: {
-        name: 'Uncryptid',
-        short_name: 'Uncryptid',
-        description: 'Cryptid game companion app',
-        theme_color: '#0e7490',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-  ],
-});
+    plugins: [
+        react(),
+        Pages(),
+        VitePWA({
+            base: '/',
+            includeAssets: [
+                'favicon.svg',
+                'favicon.ico',
+                'robots.txt',
+                'apple-touch-icon.png',
+            ],
+            manifest: {
+                name: 'Uncryptid',
+                short_name: 'Uncryptid',
+                description: 'Cryptid game companion app',
+                theme_color: '#0e7490',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        }),
+    ],
+})
